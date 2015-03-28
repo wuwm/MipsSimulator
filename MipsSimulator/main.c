2a2cf60f6298c743d2c9641bf88e17494bc2a1dc
Wu <wwm1994@gmail.com>
2015-03-28 15:41:32 +0800

    Initial Commit

---
+++ MipsSimulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MipsSimulator
+//
+//  Created by 吴未名 on 15/3/28.
+//  Copyright (c) 2015年 wuwm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
